e2383a3d96f44144bb4c7e04e9fb05f4f12f623b
Yasmin Hannah <yasminhannah1510@gmail.com>
2022-06-10 14:30:31 +0800

    knn for anar and oor

---
+++ ANAR-knn.R
@@ -0,0 +1,52 @@
+library(psych)
+library(caret)
+library(dplyr)
+library(car)
+library(Metrics)
+
+anar=read.csv("totalANAR.csv")
+dt=subset(anar,select = c(regionN,Dvrregion,IndicatN,
+                          Total.Point.estimate,Children.without.functional.difficulties.Point.estimate,
+                          Children.with.functional.difficulties.Point.estimate,Time.period))
+str(dt)
+
+indexes = createDataPartition(dt$Children.with.functional.difficulties.Point.estimate, 
+                              p = .85, list = F)
+
+set.seed(12)
+
+indexes2 = createDataPartition(boston$medv, p = .85, list = F)
+train = dt[indexes, ]
+test = dt[-indexes, ]
+
+train_x = train[, -6]
+train_x = scale(train_x)[,]
+train_y = train[,6]
+
+test_x = test[, -6]
+test_x = scale(test[,-6])[,]
+test_y = test[,6]
+
+knnmodel = knnreg(train_x, train_y)
+str(knnmodel)
+
+pred_y = predict(knnmodel, data.frame(test_x))
+
+print(data.frame(test_y, pred_y))
+
+mse = mean((test_y - pred_y)^2)
+mae = caret::MAE(test_y, pred_y)
+rmse = caret::RMSE(test_y, pred_y)
+
+cat("MSE: ", mse, "MAE: ", mae, " RMSE: ", rmse)
+#MSE:  472.2275 MAE:  14.16333  RMSE:  21.7308
+
+x = 1:length(test_y)
+
+plot(x, test_y, col = "red", type = "l", lwd=2,
+     main = "anar knn data prediction")
+lines(x, pred_y, col = "blue", lwd=2)
+legend("topright",  legend = c("original", "predicted"), 
+       fill = c("red", "blue"), col = 2:3,  adj = c(0, 0.6))
+grid()
+
